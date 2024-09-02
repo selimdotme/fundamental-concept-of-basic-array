40ec1b28e4371d531d1354e943974ac452ed71e0
Selim999-BD <selim2305101565@diu.edu.bd>
2024-09-02 14:11:03 -0400

    pactice of array

---
+++ pac-array.js
@@ -0,0 +1,20 @@
+let numbers = [1,2,3,4,5,6,7];
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[0]);
+
+
+let friends = ['selim', 'hossain', 'md', 'khan'];
+console.log(friends);
+friends[0]='reja';
+console.log(friends);
+friends.push('khan','rohim','korim');
+console.log(friends);
+friends.pop();
+console.log(friends);
+friends.shift();
+console.log(friends);
+friends.unshift('md');
+console.log(friends);
+console.log(friends.includes('md'));
+console.log(friends.indexOf('md'));
